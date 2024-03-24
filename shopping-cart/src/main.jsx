429e663d11cd41cf5e8f95fc9a402affbbf11d18
Hasan Çoban <hasancoban13579@gmail.com>
2024-03-24 13:36:32 +0300

    Encapsulate with Root component to be able to use hooks and pass data

--- shopping-cart/src/main.jsx
+++ shopping-cart/src/main.jsx
@@ -1,39 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import ErrorPage from './components/Pages/ErrorPage.jsx';
 import Store from './components/Pages/Store.jsx';
 import Homepage from './components/Homepage.jsx';
-import { 
+import ItemView from './components/Pages/ItemView.jsx';
+import { getItems } from './components/Item/getItem.js';
+import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "",
-        element: <Homepage text={"Your headphone store"} />
+const Root = () => {
+  const [data, setData] = useState([]);
 
-      },
-      {
-        path: "store",
-        element: <Store />
-      },
-    ] 
-  },
+  useEffect(() => {
+    (async function getItems() {
+      const response = await fetch('https://fakestoreapi.com/products');
+      const data = await response.json();
+      setData(data);
+    })();
+  }, [])
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "",
+          element: <Homepage text={"Your headphone store"} />
 
-]
-)
+        },
+        {
+          path: "store",
+          element: <Store data={data} />,
+        },
+        {
+          path: "store/:itemId",
+          element: <ItemView item={data} />
+        }
+      ]
+    },
+  ])
 
+  return (
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  )
+}
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+  <Root />
 )
